7dc606d92ade4711a9ab48c3fdb6e847ee51821d
Md Saqulain <62801332+mdsaqulain@users.noreply.github.com>
2024-11-03 16:32:06 +0000

    Initialize Document & Parse Data

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
-/* add your code here */
-
+document.addEventListener('DOMContentLoaded', () => {
+  const stocksData = JSON.parse(stockContent);
+  const userData = JSON.parse(userContent);
+  const deleteButton = document.querySelector ('#btnDelete');
+  const saveButton = document.querySelector ('#btnSave');
+  generateUserList(userData, stocksData);
