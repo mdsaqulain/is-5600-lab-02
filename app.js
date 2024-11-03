39a07d7f2192f350b9618189b28ed925cd13e83a
Md Saqulain <62801332+mdsaqulain@users.noreply.github.com>
2024-11-03 16:33:26 +0000

    Add Delete Button Event Listener

--- app.js
+++ app.js
@@ -4,3 +4,11 @@ document.addEventListener('DOMContentLoaded', () => {
   const deleteButton = document.querySelector ('#btnDelete');
   const saveButton = document.querySelector ('#btnSave');
   generateUserList(userData, stocksData);
+  
+  deleteButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    const userId = document.querySelector('#userID').value;
+    const userIndex = userData.findIndex(user => user.id == userId);
+    userData.splice(userIndex, 1);
+    generateUserList(userData, stocksData);
+});
